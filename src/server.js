4b6ed69fe476507c98af690cb2c986bbaeaa110f
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-09-13 11:20:20 +0530

    Add async error handler and error middleware
    Introduced a reusable asyncHandler utility for handling async route errors. Added a global error-handling middleware to standardize error responses. Also added 'zod' as a new dependency in package.json.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import todoRouter from './routes/todoRoute.js'
 dotenv.config();
 const app = express()
 app.use(express.json())
+// app.use(loggermiddleware)
 
 
 let todo = []
@@ -20,7 +21,7 @@ app.use("/api/todo",todoRouter)
 
 
 
-app.get("/api/ping", loggermiddleware, (req, res) => { // ping route (health check)
+app.get("/api/ping", loggermiddleware, (req, res,next) => { // ping route (health check)
   res.json({ ok: true, timestamp: Date.now() });
 });
 
@@ -28,6 +29,17 @@ app.get("/api/ping", loggermiddleware, (req, res) => { // ping route (health che
 app.get("/check", loggermiddleware, (req, res) => { // check route (example showing middleware + response)
   res.status(200).json({ msg: "check ok" });
 });
+
+
+app.use((err, req, res, next) => { // this is basically a middleware things which can be called by next(variable name to check for errors)
+  const status = err && (err.status || err.statusCode) ? (err.status || err.statusCode) : 500
+  
+  console.log(`ERROR DETECTED : ${err.message}`)
+  res.status(status).json({
+    error: true,
+    message: err.message || "INTERNAL ERROR"
+  })
+})
 
 app.listen(PORT, () => { console.log(`we are on the port : ${PORT}`) })
 
